968032e9530743d0aa1da98e437160e25364f85c
kimichen2907@gmail.com <kimichen2907@gmail.com>
2023-12-24 11:54:26 +0800

    init

--- yahoo_search/__init__.py
+++ yahoo_search/__init__.py
@@ -1,9 +1,20 @@
 from .core import (
     search_news,
-    weather
+    weather,
+    weather_search,
+    weather_forecast,
+    search,
+    video_search
 )
 
 __all__=[
     "search_news",
-    "weather"
+    "weather",
+    "weather_search",
+    "weather_forecast",
+    "search",
+    "video_search"
 ]
+
+__author__="Cheng"
+__version__="0.01"
